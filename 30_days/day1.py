2f0453da2e451c58c160ccd75f4ec249ec9e067f
Justin <jsarraga@gmail.com>
2019-11-25 18:27:17 -0800

    day1 test

---
+++ 30_days/day1.py
@@ -0,0 +1,26 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+f = 4
+g = 4.0
+h = 'HackerRank'
+# Read and save an integer, double, and String to your variables.
+i = input()
+d = input()
+s = input()
+i = int(i)
+f = int(f)
+d = float(d)
+g = float(g)
+s = str(s)
+h = str(h)
+# Print the sum of both integer variables on a new line.
+print(i + f)
+# Print the sum of the double variables on a new line.
+print(d + g)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(h + " " + s)
+
+
