bb2ae679ff68f2ba72af85487bde7369ea7d44da
Justin <jsarraga@gmail.com>
2019-12-09 09:53:20 -0800

    day 9: recursion

---
+++ 30_days/day9.py
@@ -0,0 +1,55 @@
+"""
+Task
+Write a factorial function that takes a positive integer,  as a parameter and prints the result of  ( factorial).
+
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of .
+
+Input Format
+
+A single integer,  (the argument to pass to factorial).
+
+Constraints
+
+Your submission must contain a recursive function named factorial.
+Output Format
+
+Print a single integer denoting .
+
+Sample Input
+3
+
+Sample Output
+6
+
+Explanation
+Consider the following steps:
+1. factorial(3) = 3 x factorial(2)
+2. factorial(2) = 2 X factorial(1)
+3. factorial(1) = 1  
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n <= 1: # defining the base case
+        return 1
+    return n * factorial(n-1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
