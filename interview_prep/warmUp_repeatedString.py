c15107aef364a27dafd2017e902cc701e2aa3742
Justin <jsarraga@gmail.com>
2019-12-10 13:23:29 -0800

    warm-up. Repeated String

--- interview_prep/warmUp_repeatedString.py
+++ interview_prep/warmUp_repeatedString.py
@@ -1,23 +1,39 @@
 """
-John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
-Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+Lilah has a string, s, of lowercase English letters that she repeated infinitely many times.
 
-For example, there are n=7 socks with colors ar = [1,2,1,2,1,3,2]. There is one pair of color 1 and one of color 2. 
-There are three odd socks left, one of each color. The number of pairs is 2.
+Given an integer, n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+
+For example, if the string s='abcac' and n=10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. 
+There are 4 occurrences of a in the substring.
 
 Function Description
-Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
-sockMerchant has the following parameter(s):
-    n: the number of socks in the pile
-    ar: the colors of each sock
+Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
 
-Sample Input
-9
-10 20 20 10 10 30 50 10 20
+repeatedString has the following parameter(s):
+    s: a string to repeat
+    n: the number of characters to consider
 
-Sample Output
-3
+Sample Input 
+aba
+10
+
+Sample Output 
+7
+
+Explanation 0
+The first n=10 letters of the infinite string are abaabaabaa. Because there are 7 a's, we print 7 on a new line.
+
+Sample Input 
+a
+1000000000000
+
+Sample Output 
+1000000000000
+
+Explanation 1
+Because all of the first n=1000000000000 letters of the infinite string are a, we print 1000000000000 on a new line.
 """
+
 #!/bin/python3
 
 import math
@@ -25,21 +41,22 @@ import os
 import random
 import re
 import sys
-from collections import Counter
 
-# Complete the sockMerchant function below.
-def sockMerchant(n, ar):
-    return (sum(ar.count(x)//2 for x in set(ar))) # iterate through the list, count colors and // 2 to pair them up. Then sum it all.
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    a_count_string = s.count('a') # counts the number of "a" in a full string,
+    a_count_substring = s[0:n%len(s)].count('a') #calculates the total amount of "a" based on the potentially partial string
+    return a_count_string * (n//len(s)) + a_count_substring ##"a" count of full string * number of string repeats + "a" count of last string.
 
-        
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    s = input()
+
     n = int(input())
 
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
+    result = repeatedString(s, n)
 
     fptr.write(str(result) + '\n')
 
